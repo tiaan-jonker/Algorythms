9c16223301cd2bc10b531502e51a108e79462995
tiaan-jonker <tiaanj06@gmail.com>
2022-06-22 08:52:03 +1200

    Palindrome count variable not needed

--- leet/6.palindrome/index.js
+++ leet/6.palindrome/index.js
@@ -3,14 +3,9 @@ function palindrome(s) {
 
   let start = 0
   let end = sanitisedStr.length - 1
-  let count = 0
 
   while (start < end) {
     if (sanitisedStr[start] !== sanitisedStr[end]) {
-      count++
-    }
-
-    if (count) {
       return false
     }
 
